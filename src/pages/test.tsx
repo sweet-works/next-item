53a7803661bd525ef888709c60e212b67b684bbe
WYJTang <643392718@qq.com>
2021-06-24 19:47:13 +0800

    路径调整

---
+++ src/pages/test.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Autor: yaojie
+ * @Date: 2021-06-24 19:39:29
+ * @LastEditors: yaojie
+ * @LastEditTime: 2021-06-24 19:45:16
+ * @Description: 
+ */
+import React from 'react';
+import '../styles/test.scss'
+export default function Text() {
+  return (
+    <div>
+    <h1>test</h1>
+    </div>
+  );
+}
+
